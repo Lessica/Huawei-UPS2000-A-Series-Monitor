2a15f76b6149494539500ca7a92f30c857e8268a
82Flex <82flex@gmail.com>
2021-08-01 06:32:14 +0800

    Initial commit

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 import socket
 
 import numpy
-from flask import Flask, jsonify
+from flask import Flask, jsonify, json
+from werkzeug.exceptions import HTTPException
 
 app = Flask(__name__)
 addr = "xtzn-raspi.local"  # noqa
-host = socket.gethostbyname(addr)
+# host = socket.gethostbyname(addr)
+host = "127.0.0.1"
 port = 51410
+
+
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    """Return JSON instead of HTML for HTTP errors."""
+    # start with the correct headers and status code from the error
+    response = e.get_response()
+    # replace the body with JSON
+    response.data = json.dumps({
+        "code": e.code,
+        "name": e.name,
+        "description": e.description,
+    })
+    response.content_type = "application/json"
+    return response
 
 
 def crc16(data: bytes):
@@ -477,4 +494,5 @@ def warnings():
 
 
 if __name__ == '__main__':
-    app.run()
+    # app.run()
+    app.run(host=addr, port=(port + 1))
